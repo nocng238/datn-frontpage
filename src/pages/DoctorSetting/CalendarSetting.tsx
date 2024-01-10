705e1b0340d4f975ad62dda42f4e453e511eeaef
ngoc238 <nmtngoc238@gmail.com>
2024-01-10 20:19:56 +0700

    removed daypilot

--- src/pages/DoctorSetting/CalendarSetting.tsx
+++ src/pages/DoctorSetting/CalendarSetting.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { DayPilotCalendar, DayPilot } from "@daypilot/daypilot-lite-react";
+// import { DayPilotCalendar, DayPilot } from "@daypilot/daypilot-lite-react";
 
 const CalendarSetting = () => {
   //   const [config, setConfig] = useState({
@@ -19,65 +19,65 @@ const CalendarSetting = () => {
   });
   const calendarRef = useRef<any>();
 
-  useEffect(() => {
-    // load event data
-    calendarRef.current.control.update({
-      startDate: "2023-12-15",
-      events: [
-        {
-          id: 1,
-          text: "Event 1",
-          start: "2023-12-02T10:30:00",
-          end: "2023-12-02T13:00:00",
-        },
-        {
-          id: 2,
-          text: "Event 2",
-          start: "2023-12-03T09:30:00",
-          end: "2023-12-03T11:30:00",
-          backColor: "#6aa84f",
-        },
-        {
-          id: 3,
-          text: "Event 3",
-          start: "2023-12-15T12:00:00",
-          end: "2023-12-15T15:00:00",
-          backColor: "#f1c232",
-        },
-        {
-          id: 4,
-          text: "Event 4",
-          start: "2023-10-01T11:30:00",
-          end: "2023-10-01T14:30:00",
-          backColor: "#cc4125",
-        },
-      ],
-    });
-  }, []);
-  const handleTimeRangeSelected = (args: any) => {
-    const dp = calendarRef.current.control;
-    // console.log(dp);/
+  // useEffect(() => {
+  //   // load event data
+  //   calendarRef.current.control.update({
+  //     startDate: "2023-12-15",
+  //     events: [
+  //       {
+  //         id: 1,
+  //         text: "Event 1",
+  //         start: "2023-12-02T10:30:00",
+  //         end: "2023-12-02T13:00:00",
+  //       },
+  //       {
+  //         id: 2,
+  //         text: "Event 2",
+  //         start: "2023-12-03T09:30:00",
+  //         end: "2023-12-03T11:30:00",
+  //         backColor: "#6aa84f",
+  //       },
+  //       {
+  //         id: 3,
+  //         text: "Event 3",
+  //         start: "2023-12-15T12:00:00",
+  //         end: "2023-12-15T15:00:00",
+  //         backColor: "#f1c232",
+  //       },
+  //       {
+  //         id: 4,
+  //         text: "Event 4",
+  //         start: "2023-10-01T11:30:00",
+  //         end: "2023-10-01T14:30:00",
+  //         backColor: "#cc4125",
+  //       },
+  //     ],
+  //   });
+  // }, []);
+  // const handleTimeRangeSelected = (args: any) => {
+  //   const dp = calendarRef.current.control;
+  //   // console.log(dp);/
 
-    dp.events.add({
-      start: args.start,
-      end: args.end,
-      id: DayPilot.guid(),
-      text: "hello",
-    });
-    console.log("args : ", args);
+  //   dp.events.add({
+  //     start: args.start,
+  //     end: args.end,
+  //     id: DayPilot.guid(),
+  //     text: "hello",
+  //   });
+  //   console.log("args : ", args);
 
-    calendarRef.current.control.update({
-      startDate: args.day,
-    });
-  };
+  //   calendarRef.current.control.update({
+  //     startDate: args.day,
+  //   });
+  // };
 
   return (
     <div className="h-[90vh]">
-      <DayPilotCalendar
+      {/* <DayPilotCalendar
         {...config}
         ref={calendarRef}
         onTimeRangeSelected={handleTimeRangeSelected}
-      />
+      /> */}
     </div>
   );
 };
